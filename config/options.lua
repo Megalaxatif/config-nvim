8ec459c63df05c3311ca85bc391e415f1f5737c6
Megalaxatif <mr.megalaxatif@gmail.com>
2025-12-01 12:04:25 +0100

    init

---
+++ config/options.lua
@@ -0,0 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.relativenumber = false
+vim.opt.number = true -- pour avoir les numéros absolus quand même
+--vim.opt.winbar = "%=%m %f"
